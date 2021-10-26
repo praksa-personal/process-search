51cfc468561d5c912e4317cb21cd56207eedd0cf
Marko Puntaric <puntaric@domain.local>
2021-10-26 12:56:11 +0200

    Current process pid

--- process.py
+++ process.py
@@ -1,12 +1,19 @@
 import psutil
+import os
 
 data = psutil.pids()
+pid_current = os.getpid()
+
+print(psutil.Process(pid_current))
 
 ##print process names
 for i in data:
     try:        
         temp = psutil.Process(i)
         if(temp.name()=='Code.exe' and temp.status()=='running'):
-            print(temp.name())
+            #print(temp.children(recursive=True),"\n")
+            print(temp.started()) #should be time, ali ne zeli
     except:
         continue
+
+
