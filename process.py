86e8064b10124e1aa733113de78bb67eb50595bf
Marko Puntaric <puntaric@domain.local>
2021-10-27 13:04:22 +0200

    Moved to func

--- process.py
+++ process.py
@@ -2,25 +2,28 @@ import psutil
 import os
 import datetime
 
-data = psutil.pids()
-pid_current = os.getpid()
 
+#pid_current = os.getpid()
 #print(psutil.Process(pid_current))
 
-##print process names
-for i in data:
-    try:        
-        temp = psutil.Process(i)
-        #if(temp.name()=='Code.exe' and temp.status()=='running'):
-            #print(temp.children(recursive=True),"\n")
-            #print(temp.started()) #should be time, ali ne zeli
-        if(temp.name()=='test_process.exe'):
-            print(temp.name())  
-            time = temp.create_time()
-            formated = datetime.datetime.fromtimestamp(time).strftime("%Y-%m-%d %H:%M:%S")
-            print(formated)
-            
-    except:
-        continue
+def start_time(process):
+    data = psutil.pids()
+    for i in data:
+        try:        
+            temp = psutil.Process(i)
+            #if(temp.name()=='Code.exe' and temp.status()=='running'):
+                #print(temp.children(recursive=True),"\n")
+                #print(temp.started()) #should be time, ali ne zeli
+            if(temp.name()==process):
+                print(temp)  
+                time = temp.create_time()
+                formated = datetime.datetime.fromtimestamp(time).strftime("%Y-%m-%d %H:%M:%S")
+                print(formated)
+                
+        except:
+            continue
 
 
+if __name__ == '__main__':
+    name = 'test_process.exe'
+    start_time(name)
