7a502231d12bc5553fc43ad730d4ed7a28b8acd2
Marko Puntaric <puntaric@domain.local>
2021-11-03 11:30:16 +0100

    Removed some code

--- main.py
+++ main.py
@@ -12,39 +12,30 @@ if __name__ == '__main__':
     client.on_publish = on_publish
     client.connect(host="localhost", port=1883)
     
-    test_pid=-1
-    flag = 0
-    
     print("Start test? [y/n]")
     x = input()
-    #start dummy test.exe
     
     if(x == 'y'):
+        #start dummy test.exe process
         os.startfile("C:/Users/puntaric/Projekti/process-search/exe/dist/test.exe")
 
-        try:
-            test_start, test_pid, test_name = on_test_start('test.exe')
-            flag = 1
-        except:
-            ()
+        test_start, test_pid, test_name = on_test_start('test.exe')
 
-        if(flag == 1):
-            procs_list = [psutil.Process(test_pid)]
-            #print("Waiting for process {} to be terminated".format(test_name))
-            # waits for process to terminate, 20 sec timeout
-            gone, alive = psutil.wait_procs(procs_list, timeout=30, callback=on_terminate)
-            x = 'n'
-            test_end = datetime.datetime.now()
-            test_end = test_end.strftime("%H:%M:%S")
-
-            #test terminated, get running time, random status, post over mqtt
-            dtime = running_time(test_start,test_end)
-            topic, res_str = simulate_results()
-            
-            msg = "Test id XX " + res_str + ", total running time:\n" + str(dtime) 
-            (rc, mid) = client.publish(topic, str(msg), qos=1)
+        procs_list = [psutil.Process(test_pid)]
+        #print("Waiting for process {} to be terminated".format(test_name))
+        # waits for process to terminate, 20 sec timeout
+        gone, alive = psutil.wait_procs(procs_list, timeout=30, callback=on_terminate)
+        x = 'n'
+        test_end = datetime.datetime.now()
+        test_end = test_end.strftime("%H:%M:%S")
+        #test terminated, get running time, random status, post over mqtt
+        dtime = running_time(test_start,test_end)
+        topic, res_str = simulate_results()
+        
+        msg = "Test id XX " + res_str + ", total running time:\n" + str(dtime) 
+        (rc, mid) = client.publish(topic, str(msg), qos=1)
              
-        else:
-            print("No running test process")
+    else:
+        print("No running test process")
 
     
